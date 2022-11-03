11912c17c1e0c4adc5963353ebc24c18ed35d950
HRodenhizer <heidi.rodenhizer@yahoo.com>
2022-11-03 11:01:56 -0700

    adds .Rproj file and initial script to install my regularly used R packages

---
+++ r_package_installs.R
@@ -0,0 +1,30 @@
+################################################################################
+###                    Initial Package Install                               ###
+###                      Code by HGR 08/2022                                 ###
+################################################################################
+
+# Install devtools to allow github installs
+install.packages("devtools")
+
+# Data management
+install.packages(c("data.table", "tidyverse", "writexl"))
+
+# Spatial Data
+install.packages(c("sp", "sf", "raster", "terra", "ggmap", "ggsn", "leaflet",
+                   "mapview", "satellite", "spatialEco", "spatstat", "stars", 
+                   'cleangeo', 'ggspatial', 'plotrix', 'rworldmap', 'ncdf4'))
+devtools::install_github("SEEG-Oxford/seegSDM")
+devtools::install_github("HRodenhizer/thermokarstdetection")
+
+# Statistics
+install.packages(c("caret", "emmeans", "gbm", "gstat", "lme4", "lmerTest",
+                   "MuMin", "tree", "treeshap", "zoo"))
+
+# Visualization
+install.packages(c("Cowplot", "DiagrammeR", "DiagrammeRsvg", "ggfortify",
+                   "ggnewscale", "ggpubr", "ggthemes", "gridExtra", "gtable",
+                   "magick", "viridis"))
+devtools::install_github('tylermorganwall/rayshader')
+
+# Other
+install.packages(c("doParallel", "neonUtilities", 'svMisc', 'kit'))
